1d0f713e7dfaa455bb847f454692964c376f61ad
R3SH <alekss-reshetov@mail.ru>
2018-08-09 00:48:44 +0400

    NoSound

--- main.cpp
+++ main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include "olcNoiseMaker.h"
 
+#define OSC_SINE 0
+#define OSC_SQUARE 1
+#define OSC_TRIANGLE 2
+#define OSC_SAW_AN 3
+#define OSC_SAW_OP 4
+#define OSC_NOISE 5
+
 double w(double dHerz)		//Converts frequency(Hz) to angular velocity
 {
 	return dHerz * 2.0 * PI;
 }
 
-double osc(double dHerz, double dTime, int Type)
+double osc(double dHertz, double dTime, int Type = OSC_SINE, double dLFOHertz = 0.0, double dLFOAmplitude = 0.0)
 {
+	double dFreq = w(dHertz) * dTime + dLFOAmplitude * dHertz * sin(w(dLFOHertz) * dTime);			//base frequency
+
 	switch (Type)
 	{
-	case 0:		//Sine wave
-		return sin(w(dHerz) * dTime);
+	case OSC_SINE:		//Sine wave
+		return sin(dFreq);
 
-	case 1:		//Sqare wave
-		return sin(w(dHerz) * dTime) > 0.0 ? 1.0 : -1.0;
+	case OSC_SQUARE:		//Sqare wave
+		return sin(dFreq) > 0.0 ? 1.0 : -1.0;
 
-	case 2:		//Triangle wave
-		return asin(sin(w(dHerz) * dTime)) * 2 / PI;
+	case OSC_TRIANGLE:		//Triangle wave
+		return asin(sin(dFreq)) * (2.0 / PI);
 
-	case 3:		//Saw wave (analogue / warm / slow)
+	case OSC_SAW_AN:		//Saw wave (analogue / warm / slow)
 	{
 		double dOutput = 0.0;
 
 		for (double n = 1.0; n < 10.0; n++)
-			dOutput += (sin(n * w(dHerz) * dTime)) / n;
+			dOutput += (sin(n * dFreq)) / n;
 
 		return dOutput * (2.0 / PI);
 	}
 
-	case 4:		//Saw wave (optimized / harsh / fast)
-		return (2.0 / PI) * (dHerz * PI * fmod(dTime, 1.0 / dHerz) - (PI / 2.0));
+	case OSC_SAW_OP:		//Saw wave (optimized / harsh / fast)
+		return (2.0 / PI) * (dHertz * PI * fmod(dTime, 1.0 / dHertz) - (PI / 2.0));
+
+	case OSC_NOISE:		//Pseudo Random Noise
+		return 2.0 * ((double)rand() / (double)RAND_MAX) - 1.0;
 
 	default:
 		return 0.0;
@@ -52,14 +64,14 @@ struct sEnvelopeADSR
 
 	sEnvelopeADSR()
 	{
-		dAttackTime = 0.100;
-		dDecayTime = 0.01;
+		dAttackTime = 0.001;
+		dDecayTime = 1.0;
+		dStartAmplitude = 1.0;
+		dSustainAmplitude = 0.0;
 		dReleaseTime = 1.0;
-		dSustainAmplitude = 0.8;
-		dStartAmplitude = 0.200;
+		bNoteOn = false;
 		dTriggerOnTime = 0.0;
 		dTriggerOffTime = 0.0;
-		bNoteOn = false;
 	}
 	double GetAmplitude(double dTime)
 	{
@@ -115,27 +127,79 @@ double dOctaveBaseFrequency = 110.0;
 double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
 sEnvelopeADSR envelope;
 
-double MakeNoise(double dTime)
+struct instrument
 {
-	double dOutput = envelope.GetAmplitude(dTime) *
-		(
-			+osc(dFrequencyOutput * 0.5, dTime, 3)
-			+ osc(dFrequencyOutput * 1.0, dTime, 1)
-		);
-		
+	double dVolume;
+	sEnvelopeADSR env;
+	virtual double sound(double dTime, double dFrequency) = 0;
+};
 
-	return dOutput * 0.1;		//Master volume
+
+struct bell : public instrument
+{
+	bell()
+	{
+		env.dAttackTime = 0.001;
+		env.dDecayTime = 1.0;
+		env.dStartAmplitude = 1.0;
+		env.dSustainAmplitude = 0.0;
+		env.dReleaseTime = 1.0;
+
+		dVolume = 1.0;
+	}
+
+	double sound(double dTime, double dFrequency)
+	{
+		double dOutput = envelope.GetAmplitude(dTime) *
+			(
+			+1.0 * osc(dFrequencyOutput * 2.0, dTime, OSC_SINE, 5.0, 0.001)
+			+ 0.5 * osc(dFrequencyOutput * 3.0, dTime, OSC_SINE)
+			+ 0.25 * osc(dFrequencyOutput * 4.0, dTime, OSC_SINE)
+			);
+
+		return dOutput;
+	}
+};
+
+instrument *voice = nullptr;
+
+double MakeNoise( double dTime)
+{
+	//double dOutput = envelope.GetAmplitude(dTime) *
+	//	(
+	//		+ 1.0 * osc(dFrequencyOutput, dTime, OSC_SQUARE, 5.0, 0.001)
+	//		+ 0.5 * osc(dFrequencyOutput * 1.5, dTime, OSC_SQUARE)
+	//		+ 0.25 * osc(dFrequencyOutput * 2.0, dTime, OSC_SQUARE)
+	//		+ 0.05 * osc(0, dTime, OSC_NOISE)		//Harmonica
+
+	//		+1.0 * osc(dFrequencyOutput * 2.0, dTime, OSC_SINE, 5.0, 0.001)
+	//		+ 0.5 * osc(dFrequencyOutput * 3.0, dTime, OSC_SINE)
+	//		+ 0.25 * osc(dFrequencyOutput * 4.0, dTime, OSC_SINE)
+	//	);
+
+	double dOutput = voice->sound(dTime, dFrequencyOutput);
+
+	return dOutput * 0.05;		//Master volume
 }
 
 int main()
 {
-	std::wcout << "Synthesizer" << std::endl;
+	wcout << "Synthesizer" << std::endl;
 
-	vector<std::wstring> devices = olcNoiseMaker<short>::Enumerate();
+	std::vector<std::wstring> devices = olcNoiseMaker<short>::Enumerate();
 
-	for (auto d : devices) wcout << "Found output device: " << d << std::endl;
+	wcout << endl <<
+		"|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |" << endl <<
+		"|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |" << endl <<
+		"|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__" << endl <<
+		"|     |     |     |     |     |     |     |     |     |     |" << endl <<
+		"|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
+		"|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
+
 
 	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+
+	voice = new bell();
 
 	sound.SetUserFunction(MakeNoise);
 
@@ -155,8 +219,8 @@ int main()
 				if (nCurrentKey != k)
 				{
 					dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
-					envelope.NoteOn(sound.GetTime());
-					//wcout << "r\NoteOn : " << sound.GetTime() << "s; Frequency: " << dFrequencyOutput << std::endl;
+					voice->env.NoteOn(sound.GetTime());
+					wcout << "\rNoteOn : " << sound.GetTime() << "s; Frequency: " << dFrequencyOutput << "Hz";
 					nCurrentKey = k;
 				}
 
@@ -168,8 +232,8 @@ int main()
 		{
 			if (nCurrentKey != -1)
 			{
-				//wcout << "\rNoteOff: " << sound.GetTime << "s" << std::endl;
-				envelope.NoteOff(sound.GetTime());
+				wcout << "\rNoteOff: " << sound.GetTime() << "s					";
+				voice->env.NoteOff(sound.GetTime());
 				nCurrentKey = -1;
 			}
 		}
@@ -177,3 +241,4 @@ int main()
 
 	return 0;
 }
+
